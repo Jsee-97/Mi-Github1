7c65f7cf67cdecea7c7e454da176b2d090af4bfd
Oscar Morao <moraolup@gmail.com>
2020-09-05 19:55:38 -0300

    Commit de nombre viejos de archivos

--- Prueba1/prueba2.py
+++
@@ -1,9 +0,0 @@
-#!usr/bin/python3
-
-nombre=input("escribe tu nombre: ")
-CPF=int(input("escribe tu CPF: "))
-edad=int(input("escribe tu edad: ")) 
-
-
-print("Confirmacion de registro: ")
-print(f"Tu nombre es: {nombre} \nTu CPF es: {CPF} \nTu edad es: {edad}")
