ab5f60160f1925bd73194a6a8f1f086a05c9ba4b
Oscar Morao <moraolup@gmail.com>
2020-09-05 19:38:42 -0300

    Cambiando nombre de carpetas para organizar

---
+++ material_4520/Josuee/prueba2.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+
+nombre=input("escribe tu nombre: ")
+CPF=int(input("escribe tu CPF: "))
+edad=int(input("escribe tu edad: ")) 
+
+
+print("Confirmacion de registro: ")
+print(f"Tu nombre es: {nombre} \nTu CPF es: {CPF} \nTu edad es: {edad}")
