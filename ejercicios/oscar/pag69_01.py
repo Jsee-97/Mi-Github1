405e0d980c66f0c6b39d26e6808bc151683804fe
Oscar Morao <moraolup@gmail.com>
2020-10-06 17:38:42 -0300

    Ejercicio listo

--- ejercicios/oscar/pag69_01.py
+++ ejercicios/oscar/pag69_01.py
@@ -2,14 +2,13 @@
 1) Escreva uma função que receba um nome e que tenha como saída uma saudação. O argumento da função deverá ser o nome, e saída deverá ser como a seguir:
 chamada da função: saudacao('Lalo')
 saída: 'Olá Lalo! Tudo bem com você?'
-) Reescreva o exercício da quitanda do capítulo 2 separando as operações em funções.
 '''
 
 def saludo(arg):
     print(f'Hola {arg}, todo bien y tú?')
 
 
-name = input('Ingresa tu nombre')
+name = input('Ingresa tu nombre: ')
 
 saludo(name)
 
