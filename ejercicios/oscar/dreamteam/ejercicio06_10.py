9652ab5e1427d0051b1f39f3deb8d4c8f0c4d89b
Oscar Morao <moraolup@gmail.com>
2020-10-08 11:53:28 -0300

    Ejercicio ASCII terminado y agregué información de como comentar varias líneas en vim

--- ejercicios/oscar/dreamteam/ejercicio06_10.py
+++ ejercicios/oscar/dreamteam/ejercicio06_10.py
@@ -8,63 +8,70 @@ Imprime la cadena en mayúscula.
 Aquí hay una muestra de cómo ejecutar este programa.
 '''
 
-#Crear lista
 
-#Solicitar cadena de texto
-string = input("Escriba el texto que será usado: ")
+#Variables
 lista = []
+contenedor = []
 lista2 = []
-lista3 = []
-lista4 = []
+cant_num = []
+suma_lista = []
 x = 0
-lista_asc = []
-cant = int(input("Cantidad de dígitos: "))
-#taman = len(var)
+lista_ch = []
 
-#Imprimir 4 primeros caracteres en Mayúsculas
-print('Mayúsculas con función automática: \n', str.upper(string[0:cant]) + string[cant:],'\n')
+#Funciones
+#Convertir Texto a ASCII
+def text_ascii(s,l):
+    for i in s:
+        asc = ord(i)
+        l.append(asc)
+
+#Solicitar cadena de texto y cantidad de dígitos al usuário
+string = input("Escriba el texto que será usado: ")
+cant = int(input("Cantidad de dígitos: "))
+
+
+#Imprimir 4 primeros caracteres en Mayúsculas usando función de Python3
+print('\nMayúsculas con función automática: \n', str.upper(string[0:cant]) + string[cant:],'\n')
 
 
 #identificar equivalentes en ASCII
-for c in string:
-    asc = ord(c)
-    lista.append(asc)
-  
-#print(f'Lista normal en ASCII \n{lista}\n')
+text_ascii(string,lista)
+ 
 
 #obtener lista ASCII con las mayúsculas
-
 for c in range(cant):
     asc = str.upper(string[x])
     x += 1
-    lista2.append(asc)
+    contenedor.append(asc)
 
-string2 = ''.join(lista2)
+string2 = ''.join(contenedor)
+text_ascii(string2, lista2)
+print(f'\nLista en minúsculas ASCII:\n{lista}\n')
 
-for c in string2:
-    ascc = ord(c)
-    lista3.append(ascc)
 
-print(lista)
-print(lista3)
+#Calcular cantidad de números entre mayúsculas y minúsculas ASCII
+for i in range(cant):
+    cant_num.append(lista[i] - lista2[i])
 
+print(f'\nDiferencia entre caracteres:\n {cant_num}\n')
+
+#Calcular lista final en ASCII
+for i in range(cant):
+    lista[i] = lista[i] - cant_num[i]
+
+
+print(f'\nLista en Mayúsculas ASCII:\n {lista2}\n')
+print(f'\nLista final en ASCII:\n {lista}\n')
 
 #Transformar de ACSII a Carácter
 for c in lista:
     ch = chr(c)
-    lista_asc.append(ch)
-
-#Sumar o restar ASCII para cambiar valores a Mayúsculas
-lista4 = map(sum, (lista, lista3))
-print(lista4)
-
+    lista_ch.append(ch)
 
 #concatenando la lista
-#conc = ''.join(lista_asc)
-#print(conc)
+result = ''.join(lista_ch)
+print(f'\nLista en Caracteres usando la lista transformada:\n{result}')
 
-#Imprimir cadena en Carácter
-#print(lista_asc)
 
 exit()
 
