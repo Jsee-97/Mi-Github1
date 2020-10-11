a94583035dc2878051cf37ca25c3dface5ef3883
oscarmoraog <moraolup@gmail.com>
2020-10-11 20:23:44 -0300

    Subiendo ejercicio 2 del dream team

---
+++ ejercicios/ejercicio_comun_01.py
@@ -0,0 +1,9 @@
+'''
+Escriba un programa que ponga en mayúscula los primeros cuatro caracteres de una cadena convirtiendo los caracteres a su equivalente ASCII,
+
+Luego agregue la cantidad necesaria para ponerlos en mayúscula y vuelva a convertir los números enteros en caracteres.
+
+Imprime la cadena en mayúscula.
+
+Aquí hay una muestra de cómo ejecutar este programa.
+'''
