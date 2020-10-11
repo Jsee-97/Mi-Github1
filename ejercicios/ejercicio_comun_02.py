6cb5839e055b314477831e1bd9c3e3eeefff2ac2
oscarmoraog <moraolup@gmail.com>
2020-10-11 20:23:02 -0300

    Ejercicio 2

---
+++ ejercicios/ejercicio_comun_02.py
@@ -0,0 +1,6 @@
+'''
+1) Toma cualquier número entero que no sea negativo y asignale el nombre de C0.
+2) Si es par, evalúa un nuevo C0 como C0 + 2
+3) De lo contrario, si es impar, evalúe un nuevo C0 como 3 x C0 + 1
+Si C0 != 1, salta al punto 2
+'''
